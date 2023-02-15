7f5f59e41578bdbb751609ba58b89de3a7334d33
nhungnq2000 <55863320+alannguyen2705@users.noreply.github.com>
2023-02-15 19:05:49 +0700

    Add files via upload

---
+++ 02a_Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("-----Basic-----");
+string name = "Manh";
+int age = 18;
+Console.WriteLine($"Name: {name} - Age: {18} ");
+
+Console.WriteLine("\n------");
+Employee mike = new() { Id = 1, Name = "Mike", Salary = 1000m };
+Console.WriteLine($"Id = {mike.Id} - Name = {mike.Name} - Salary = {mike.Salary}");
+
+Console.WriteLine("-----Copy Value-----");
+string name2 = name;
+Console.WriteLine($"name: {name} - name2 = {name2}");
+name2 = "Kha";
+Console.WriteLine($"name: {name} - name2 = {name2}");
+
+int age2 = age;
+Console.WriteLine($"age: {age} - age2: {age2}");
+age2 = 16;
+Console.WriteLine($"age: {age} - age2: {age2}");
+
+Console.WriteLine("\n------");
+Employee pole = mike;
+Console.WriteLine("Before");
+Console.WriteLine($"Id: {mike.Id} - Name: {mike.Name} - Salary: {mike.Salary}");
+Console.WriteLine($"Id: {pole.Id} - Name: {pole.Name} - Salary: {pole.Salary}");
+
+pole.Id = 2;
+pole.Name = "Pole";
+pole.Salary = 2000m;
+Console.WriteLine("After");
+Console.WriteLine($"Id: {mike.Id} - Name: {mike.Name} - Salary: {mike.Salary}");
+Console.WriteLine($"Id: {pole.Id} - Name: {pole.Name} - Salary: {pole.Salary}");
+
+class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+}
---
+++ 02b_Program.cs
@@ -0,0 +1,50 @@
+﻿Student s1 = new()
+{
+    Id = 1,
+    Name = "A",
+    Mark = 10f,
+    HomeAddress = new() 
+    { 
+        City = "Ho Chi Minh", District = "Quan Binh Thanh", Street = "Bach Dang" 
+    }
+};
+
+Student s2 = new() { Id = 2, Name = "B", Mark = 9f};
+s2.HomeAddress = s1.HomeAddress;
+Console.WriteLine("Before");
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+
+s2.HomeAddress.City = "Ha Noi";
+s2.HomeAddress.Street = "Tran Duy Hung";
+s2.HomeAddress.District = "Cau Giay District";
+Console.WriteLine("\nAfter first time");
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+
+s2.HomeAddress = new Address
+{
+    City = "Lam Dong",
+    Street = "Duong vao tim em",
+    District = "Hoai Duc"
+};
+Console.WriteLine("\nAfter second time");
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Mark { get; set; }
+    public Address HomeAddress { get; set; }
+    public override string ToString() => $"Id: {Id} - Name = {Name} - Mark = {Mark} - Address = {HomeAddress}";
+}
+class Address
+{
+    public string City { get; set; }
+    public string District { get; set; }
+    public string Street { get; set; }
+    public override string ToString() => $"City: {City} - District: {District} - Street: {Street}";
+
+}
